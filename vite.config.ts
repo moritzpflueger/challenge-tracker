7e4b8d20d120692cf22c24370cb5c2ccbb23c33c
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-07 21:22:50 +0100

    introduce PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,6 +7,25 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Challenge Counter',
+        short_name: 'Counter',
+        theme_color: '#000',
+        background_color: '#000',
+        icons: [
+          {
+            "src": "favicon.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+        ],
+      },
+    }),
     vue({
       template: {
         compilerOptions: {
