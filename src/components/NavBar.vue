ad6f0a28c39bde182a4fed39c4e85bc2d117cb0a
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-03-15 11:02:12 +0100

    polish add counter button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <header class="flex justify-between items-center p-5 pb-0">
     <router-link 
       to="/new-counter"
-      class="bg-neutral-800 text-neutral-400 w-12 h-12 font-semibold rounded-xl flex justify-center items-center"
+      class="text-neutral-400 w-12 h-12 font-semibold rounded-xl flex justify-center items-center"
     >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
