6cbb774b1646747935ce7dc4df069ca63e8c669c
Moritz Pflüger <moritzpflueger@googlemail.com>
2025-06-24 16:21:41 +0200

    Apply auto formatting

--- src/components/CounterItem.vue
+++ src/components/CounterItem.vue
@@ -1,74 +1,73 @@
 <template>
-  <div 
+  <div
     ref="counterRef"
     class="flex w-full flex-col items-center justify-center text-neutral-200 p-10 lg:p-64 relative rounded-xl"
-    :class="[isFinished(counter) ? 'bg-green-800 ': 'bg-neutral-900']"
+    :class="[isFinished(counter) ? 'bg-green-800 ' : 'bg-neutral-900']"
   >
-    <ButtonDelete 
+    <ButtonDelete
       @delete="$emit('delete', counter.id)"
       :isFinished="isFinished(counter)"
     />
     <h1 class="text-xl mb-5">
-      {{ isFinished(counter) ? '🎉🎉🎉' : counter.title }}
+      {{ isFinished(counter) ? "🎉🎉🎉" : counter.title }}
     </h1>
-    <p class="flex flex-col font-bold text-white text-8xl justify-center items-center my-10">
+    <p
+      class="flex flex-col font-bold text-white text-8xl justify-center items-center my-10"
+    >
       {{ counter.count }}
-      <span 
+      <span
         v-if="!isFinished(counter)"
         class="text-2xl font-normal text-neutral-500"
       >
         {{ counter.target }}
       </span>
     </p>
-    <ProgressBar 
-      v-if="!isFinished(counter)"
-      :counter="counter" 
-    />
+    <ProgressBar v-if="!isFinished(counter)" :counter="counter" />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { ref, onMounted, onUnmounted } from 'vue';
-  import type { Counter } from '@/types';
-  import Hammer, { DIRECTION_ALL } from 'hammerjs';
-  import { useCounterStore } from '@/stores/counter';
-  import ProgressBar from '@/components/ProgressBar.vue';
-  import ButtonDelete from '@/components/ButtonDelete.vue';
+import { ref, onMounted, onUnmounted } from "vue";
+import type { Counter } from "@/types";
+import Hammer, { DIRECTION_ALL } from "hammerjs";
+import { useCounterStore } from "@/stores/counter";
+import ProgressBar from "@/components/ProgressBar.vue";
+import ButtonDelete from "@/components/ButtonDelete.vue";
 
-  const props = defineProps<{
-    counter: Counter;
-  }>();
+const props = defineProps<{
+  counter: Counter;
+}>();
 
-  const store = useCounterStore();
-  const hammerInstances = ref<HammerManager[]>([]);
-  const counterRef = ref(null)
+const store = useCounterStore();
+const hammerInstances = ref<HammerManager[]>([]);
+const counterRef = ref(null);
 
-  const isFinished = (counter: Counter) => counter.count >= (counter.target ?? 0);
+const isFinished = (counter: Counter) => counter.count >= (counter.target ?? 0);
 
-  onMounted(() => {
-    let element = counterRef.value;
-    if (element) {
-      let hammertime = new Hammer(element);
-      hammertime.get('swipe').set({ direction: DIRECTION_ALL });
-      hammertime.get('press').set({ time: 350 });
+onMounted(() => {
+  let element = counterRef.value;
+  if (element) {
+    let hammertime = new Hammer(element);
+    hammertime.get("swipe").set({ direction: DIRECTION_ALL });
+    hammertime.get("press").set({ time: 350 });
 
-      hammertime.on('tap', (event: any) => {
-        if (event.target.closest('.delete-button')) return
-        event.preventDefault()
-        store.increment(props.counter)
-      });
-      hammertime.on('press', (event) => {
-        event.preventDefault()
-        store.increment(props.counter, true)
-      });
-      hammertime.on('swipedown', () => store.decrement(props.counter));
-      hammertime.on('swipeup', () => store.increment(props.counter));
-      hammerInstances.value.push(hammertime);
-    }
-  });
+    hammertime.on("tap", (event: any) => {
+      if (event.target.closest(".delete-button")) return;
+      event.preventDefault();
+      store.increment(props.counter);
+    });
+    hammertime.on("press", (event) => {
+      event.preventDefault();
+      store.increment(props.counter, true);
+    });
+    hammertime.on("swipedown", () => store.decrement(props.counter));
+    hammertime.on("swipeup", () => store.increment(props.counter));
+    hammerInstances.value.push(hammertime);
+  }
+});
 
-  onUnmounted(() => {
-    hammerInstances.value.forEach((instance: any) => instance.destroy());
-    hammerInstances.value = [];
-  });
+onUnmounted(() => {
+  hammerInstances.value.forEach((instance: any) => instance.destroy());
+  hammerInstances.value = [];
+});
 </script>
