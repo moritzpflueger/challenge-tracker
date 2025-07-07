f9a74fd24af39b86d92a927be40001d7d6fcae84
Moritz Pflüger <moritzpflueger@googlemail.com>
2025-07-07 12:37:11 +0200

    Apply auto formatting

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,46 +2,53 @@ import { ref } from "vue";
 import { defineStore } from "pinia";
 import type { Counter } from "@/types";
 
-export const useCounterStore = defineStore('counter', () => {
-  const counters = ref([] as Counter[])
+export const useCounterStore = defineStore("counter", () => {
+  const counters = ref([] as Counter[]);
   try {
-    counters.value = JSON.parse(localStorage.getItem('counters') || '[]')
+    counters.value = JSON.parse(localStorage.getItem("counters") || "[]");
   } catch (error) {
-    console.error('Error parsing counters from localStorage:', error);
-    counters.value = []; // Default to an empty array if parsing fails    
+    console.error("Error parsing counters from localStorage:", error);
+    counters.value = []; // Default to an empty array if parsing fails
   }
-  
+
   const increment = (counter: Counter, customStep = false) => {
-    if (!counter.target) counter.target = 0
-    if (counter.count >= counter.target) return
-    customStep ? counter.count += 5 : counter.count++
-    persistToLocalStorage()
-  }
+    if (!counter.target) counter.target = 0;
+    if (counter.count >= counter.target) return;
+    customStep ? (counter.count += 5) : counter.count++;
+    persistToLocalStorage();
+  };
 
   const decrement = (counter: Counter, customStep = false) => {
-    if (counter.count <= 0) return
-    customStep ? counter.count -= 5 : counter.count--
-    persistToLocalStorage()
-  }
-  
+    if (counter.count <= 0) return;
+    customStep ? (counter.count -= 5) : counter.count--;
+    persistToLocalStorage();
+  };
+
   const updateCount = (counter: Counter, newCount: number) => {
-    counter.count = newCount
-    persistToLocalStorage()
-  }
+    counter.count = newCount;
+    persistToLocalStorage();
+  };
 
   const addCounter = (counter: Counter) => {
-    counters.value.push(counter)
-    persistToLocalStorage()
-  }
+    counters.value.push(counter);
+    persistToLocalStorage();
+  };
 
   const deleteCounter = (id: string) => {
-    counters.value = counters.value.filter((counter) => counter.id !== id)
-    persistToLocalStorage()
-  }
+    counters.value = counters.value.filter((counter) => counter.id !== id);
+    persistToLocalStorage();
+  };
 
   const persistToLocalStorage = () => {
-    localStorage.setItem('counters', JSON.stringify(counters.value))
-  }
+    localStorage.setItem("counters", JSON.stringify(counters.value));
+  };
 
-  return { counters, increment, decrement, updateCount, addCounter, deleteCounter }
-})
+  return {
+    counters,
+    increment,
+    decrement,
+    updateCount,
+    addCounter,
+    deleteCounter,
+  };
+});
